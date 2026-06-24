ea88bb2f80fe743a167343e6d5603624fabeaf54
finaunzila <finaunzila@gmail.com>
2026-06-24 18:31:30 +0700

    fix: force add bootstrap cache files for Vercel

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,191 @@
+<?php return array (
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+    ),
+  ),
+  'brackets/admin-auth' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminAuth\\AdminAuthServiceProvider',
+    ),
+  ),
+  'brackets/admin-listing' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminListing\\AdminListingServiceProvider',
+    ),
+  ),
+  'brackets/admin-translations' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminTranslations\\AdminTranslationsServiceProvider',
+    ),
+  ),
+  'brackets/admin-ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminUI\\AdminUIServiceProvider',
+    ),
+  ),
+  'brackets/advanced-logger' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdvancedLogger\\AdvancedLoggerServiceProvider',
+    ),
+  ),
+  'brackets/craftable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\Craftable\\CraftableServiceProvider',
+    ),
+  ),
+  'brackets/media' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\Media\\MediaServiceProvider',
+    ),
+  ),
+  'brackets/translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laravel/legacy-factories' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Database\\Eloquent\\LegacyFactoryServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-backup' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Backup\\BackupServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+  'spatie/laravel-medialibrary' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\MediaLibrary\\MediaLibraryServiceProvider',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-signal-aware-command' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\SignalAwareCommand\\SignalAwareCommandServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Signal' => 'Spatie\\SignalAwareCommand\\Facades\\Signal',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+);
